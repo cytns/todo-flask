f0acacd8c44db50b43e22d4cef80bffc455f6f1c
cytns <mail@eivchenko.com>
2026-09-23 14:11:28 +0300

    Добавил создание и список задач

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+tasks = []
 
 
 @app.route("/")
 def index():
-    return "Todo работает!"
+    return render_template("index.html", tasks=tasks)
+
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    task = request.form["task"]
+
+    if task:
+        tasks.append(task)
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
